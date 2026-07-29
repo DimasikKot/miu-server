04ea24a62c483bbe24848764a0cc09cef1cb75a7
Кожухарев Дмитрий <dima.kozhuharew@yandex.ru>
2026-07-29 23:20:45 +0500

    fix

--- app/diff.py
+++ app/diff.py
@@ -47,13 +47,10 @@ def build_download_list(
 
     client_pack = client_manifest.pack
     server_pack = server_manifest.pack
-    if (
-        server_pack is not None
-        and client_pack is not None
-        and client_pack.sha256 != server_pack.sha256
+    if client_instance is None or (
+        server_pack is not None and client_pack.sha256 != server_pack.sha256
     ):
-
-        # SHA отличается
+        # SHA отличается или файл отсутствует у клиента
         download.append(
             DownloadFile(
                 path=server_pack.path,
@@ -67,13 +64,10 @@ def build_download_list(
 
     client_instance = client_manifest.instance
     server_instance = server_manifest.instance
-    if (
-        server_instance is not None
-        and client_instance is not None
-        and client_instance.sha256 != server_instance.sha256
+    if client_instance is None or (
+        server_instance is not None and client_instance.sha256 != server_instance.sha256
     ):
-
-        # SHA отличается
+        # SHA отличается или файл отсутствует у клиента
         download.append(
             DownloadFile(
                 path=server_instance.path,
