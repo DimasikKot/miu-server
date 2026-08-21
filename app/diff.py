ea91140375d40e4ae6ad342b057c13343b808620
Кожухарев Дмитрий <dima.kozhuharew@yandex.ru>
2026-08-21 22:19:37 +0500

    lint: оптимизировал создание ссылки на загрузку

--- app/diff.py
+++ app/diff.py
@@ -9,8 +9,8 @@ from models.ServerInfo import ServerInfo
 from config import settings
 
 
+# Проверяет, считается ли данный SHA удалённым
 def is_deleted(path: str, sha256: str, instance_manifest: InstanceManifest) -> bool:
-    # Проверяет, считается ли данный SHA удалённым
     deleted = instance_manifest.deleted.get(path, [])
     return sha256 in deleted
 
@@ -18,24 +18,22 @@ def is_deleted(path: str, sha256: str, instance_manifest: InstanceManifest) -> b
 def build_delete_list(
     request: UpdatePostRequest, instance_manifest: InstanceManifest
 ) -> set[str]:
-    delete: set[str] = set()
+    need_delete: set[str] = set()
     for request_file in request.files:
         request_path = request_file.path
-        # Если файла больше нет на сервере,
-        # но его SHA находится в removed
+        # Если файла больше нет на сервере, но его SHA находится в removed
         if request_path not in [
             instance_file.path for instance_file in instance_manifest.files
         ]:
             if is_deleted(request_path, request_file.sha256, instance_manifest):
-                delete.add(request_path)
+                need_delete.add(request_path)
             continue
 
-        # Если SHA клиента совпадает
-        # с одним из старых SHA
+        # Если SHA клиента совпадает с одним из старых SHA
         if is_deleted(request_path, request_file.sha256, instance_manifest):
-            delete.add(request_path)
+            need_delete.add(request_path)
 
-    return delete
+    return need_delete
 
 
 def build_download_list(
@@ -43,51 +41,46 @@ def build_download_list(
     instance_manifest: InstanceManifest,
     instance_name: str,
     base_url: str,
-) -> list[FileDownloadInfo]:
-    download: list[FileDownloadInfo] = []
+) -> set[FileDownloadInfo]:
+    need_download: set[FileDownloadInfo] = set()
     request_files_paths = {file.path for file in request.files}
 
-    # Проходим по всем файлам внутри Instance на сервере
+    # Проходим по всем файлам внутри InstanceManifest
     for instance_file in instance_manifest.files:
-        instance_path = instance_file.path
+        instance_file_path = instance_file.path
+        download_url = f"{base_url}{settings.INSTANCES_FOLDER_PATH}/{quote(instance_name)}/{quote(instance_file_path, safe='/')}"
         # Нет файла
-        if instance_path not in request_files_paths:
-            url = (
-                f"{base_url}{settings.INSTANCES_FOLDER_PATH}/"
-                f"{quote(instance_name)}/"
-                f"{quote(instance_path, safe='/')}"
-            )
-            download.append(
+        if instance_file_path not in request_files_paths:
+            need_download.add(
                 FileDownloadInfo(
-                    path=instance_path,
+                    path=instance_file_path,
                     sha256=instance_file.sha256,
                     size=instance_file.size,
-                    url=url,
+                    url=download_url,
                 )
             )
             continue
 
-        request_file = [file for file in request.files if file.path == instance_path][0]
-
+        request_file = [
+            file for file in request.files if file.path == instance_file_path
+        ][0]
         # SHA совпадает
         if request_file.sha256 == instance_file.sha256:
             continue
 
         # SHA отличается и это строгое место (моды)
-        strict_folders = ["minecraft/mods"]
-        if any(instance_path.startswith(folder) for folder in strict_folders):
-            download.append(
+        strict_folders = ["minecraft/mods"]  # TODO
+        if any(instance_file_path.startswith(folder) for folder in strict_folders):
+            need_download.add(
                 FileDownloadInfo(
-                    path=instance_path,
+                    path=instance_file_path,
                     sha256=instance_file.sha256,
                     size=instance_file.size,
-                    url=f"{base_url}{settings.INSTANCES_FOLDER_PATH}/"
-                    f"{quote(instance_name)}/"
-                    f"{quote(instance_path, safe='/')}",
+                    url=download_url,
                 )
             )
 
-    return download
+    return need_download
 
 
 def compare_servers(
@@ -110,7 +103,7 @@ def compare_servers(
     return result
 
 
-def compare_resource_packs(
+def compare_resourcepacks(
     request_resourcepacks: set[str], instance_resourcepacks: set[str]
 ) -> set[str]:
     # 1. Создаём копию списка клиента, чтобы не изменять исходный массив
@@ -138,14 +131,18 @@ def compare(
     instance_name: str,
     base_url: str,
 ) -> UpdatePostResponse:
-    download = build_download_list(request, instance_manifest, instance_name, base_url)
-    delete = build_delete_list(request, instance_manifest)
+    new_resourcepacks = compare_resourcepacks(
+        request.resourcepacks, instance_manifest.resourcepacks
+    )
+    new_servers = compare_servers(request.servers, instance_manifest.servers)
+    need_delete = build_delete_list(request, instance_manifest)
+    need_download = build_download_list(
+        request, instance_manifest, instance_name, base_url
+    )
 
     return UpdatePostResponse(
-        new_resourcepacks=compare_resource_packs(
-            request.resourcepacks, instance_manifest.resourcepacks
-        ),
-        new_servers=compare_servers(request.servers, instance_manifest.servers),
-        need_delete=set(delete),
-        need_download=set(download),
+        new_resourcepacks=new_resourcepacks,
+        new_servers=new_servers,
+        need_delete=need_delete,
+        need_download=need_download,
     )
