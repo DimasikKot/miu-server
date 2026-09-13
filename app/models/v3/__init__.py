5ac88a13a2c63ea75a4351d62f45a63ccbaa7963
Кожухарев Дмитрий <dima.kozhuharew@yandex.ru>
2026-09-13 15:10:28 +0500

    fix: забыл убрать из init пару моделей

--- app/models/v3/__init__.py
+++ app/models/v3/__init__.py
@@ -1,17 +1,7 @@
-from .FileDownloadInfo import FileDownloadInfo
-from .FileInfo import FileInfo
-from .MiuClientGetResponse import MiuClientGetResponse
-from .ServerInfo import ServerInfo
-from .UpdateGetResponse import UpdateGetResponse
 from .UpdatePostRequest import UpdatePostRequest
 from .UpdatePostResponse import UpdatePostResponse
 
 __all__ = [
-    "FileDownloadInfo",
-    "FileInfo",
-    "MiuClientGetResponse",
-    "ServerInfo",
-    "UpdateGetResponse",
     "UpdatePostRequest",
     "UpdatePostResponse",
 ]
